ee6d68b3e76beb3942549ac72db75f084a303e33
Freelancer Daud <freelancerdaud0@gmail.com>
2025-06-16 22:08:52 +0600

    added

--- myapp/resources/views/auth/register.blade.php
+++ myapp/resources/views/auth/register.blade.php
@@ -69,7 +69,7 @@
         <hr class="mt-4">
         <div class="flex items-center justify-center mt-4">
                 <a class=" text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-hidden focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                    {{ __("Login...!") }}
+                    {{ __("Login ?") }}
                 </a>
         </div>
     </x-authentication-card>
